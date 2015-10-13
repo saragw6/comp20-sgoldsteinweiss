18720bb8ee55472c96509849af2ab639183e43ba
Sara <saragw6@gmail.com>
2015-10-13 18:36:02 -0400

    Completed Lab 5

---
+++ duckhunt/README.txt
@@ -0,0 +1,22 @@
+#duckhunt
+
+----------------------
+Successes and Faliures
+----------------------
+
+I completed the following aspects successfully:
+•modified game.js file to draw the background and two ducks on the canvas
+•did not modify HTML, CSS, or sprite sheet files
+•used drawImage() and getContext() succesfully
+
+And did not implement anything incorrectly, as far as I know.
+
+-------------
+Collaborators
+-------------
+None
+
+-----
+Hours
+-----
+2
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,19 @@
-// Replace this with your code...
+function init() {
+    var canvas = document.getElementById('game_canvas');
+    var ctx = game_canvas.getContext('2d');
+
+    var birds = new Image();
+    birds.src = "duckhunt_various_sheet.png";
+
+    var bg = new Image();
+    bg.src = "duckhunt-background.gif";
+
+    function customDraw() {
+        ctx.drawImage(bg, 0, 0, 256, 240);
+        ctx.drawImage(birds, 0, 115, 35, 35, 100, 50, 35, 35);
+        ctx.drawImage(birds, 210, 115, 35, 35, 200, 75, 35, 35);
+    }
+    
+    bg.onload = customDraw;
+
+}

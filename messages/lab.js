4d575fc47c57acae1710eca697e8aa3143cb8278
Sara <saragw6@gmail.com>
2015-10-22 14:44:14 -0400

    got rid of function that was never called

--- messages/lab.js
+++ messages/lab.js
@@ -1,25 +1,23 @@
 function parse() {
-	var request = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
 
-	function getMessages(){
-		request.open("get","data.json",true);
+    request.open("get", "data.json", true);
 
-		request.onreadystatechange = function(){
-			if (request.readyState == 4) {
-				console.log("Got the data back!");
-				data = request.responseText;
-				console.log(data);
-				parsedObjects = JSON.parse(data);
-				elem = document.getElementById("messages");
-				for (count = 0; count < parsedObjects.length; count++) {
-					console.log(Object.keys(parsedObjects[count]));
-					elem.innerHTML += "<p><span = username>" + parsedObjects[count]["username"] + ": </span>" + parsedObjects[count]["content"] + "</p>";
-			}
-		}
-			else {
-				console.log("In progress...");
-			}
-		};
-		request.send(null);
-	}
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            console.log("Got the data back!");
+            data = request.responseText;
+            console.log(data);
+            parsedObjects = JSON.parse(data);
+            elem = document.getElementById("messages");
+            for (count = 0; count < parsedObjects.length; count++) {
+                console.log(Object.keys(parsedObjects[count]));
+                elem.innerHTML += "<p><span = username>" + parsedObjects[count]["username"] + ": </span>" + parsedObjects[count]["content"] + "</p>";
+        }
+    }
+        else {
+            console.log("In progress...");
+        }
+    };
+    request.send(null);
 }
